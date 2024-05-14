e926c17dcfa8a3b9d2058286f6b5f2ae677f843b
Kevin Seon <mwseon@gmail.com>
2024-05-13 21:00:43 -0500

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="justify-center flex items-center h-screen">
+    <div class="text-center">
+        <h1 class="text-8xl font-bold mb-4 animate-gradient bg-clip-text text-transparent">Hi!</h1>
+        <p class="text-4xl animate-gradient bg-clip-text text-transparent">
+            This website is down for now* :3
+        </p>
+    </div>
+</div>
+
+<style>
+    @keyframes gradient {
+        0% {
+            background-position: 0% 50%;
+        }
+        50% {
+            background-position: 100% 50%;
+        }
+        100% {
+            background-position: 0% 50%;
+        }
+    }
+
+    .animate-gradient {
+        background-image: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+        background-size: 400% 400%;
+        animation: gradient 7s linear infinite;
+    }
+</style>
